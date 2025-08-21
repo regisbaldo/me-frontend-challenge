e7489583d2e0d5acd945698e1a27fff4baa3e2ee
Reginaldo Simon Bonaldi <reginaldobonaldi22@gmail.com>
2025-08-21 16:36:24 -0300

    chore: initial commit, bootstrap vue and  initial styles (wip)

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main-container">
+ss
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+.main-container {
+  background-color: get-color('primary', 50);
+}
+</style>
