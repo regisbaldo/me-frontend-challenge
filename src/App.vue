b5ce656ac1eea55136b0cba8c2194f111b8b2f9c
Reginaldo Simon Bonaldi <reginaldobonaldi22@gmail.com>
2025-08-26 16:50:49 -0300

    chore: change geral container padding o responsive devices

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,16 @@ import DetailContainer from './containers/DetailContainer.vue';
 
 <style lang="scss">
 .main-container {
-  margin: 25px;
+  margin: 25px 25px 150px 25px;
   background: $base;
-  height: 100vh;
+  height: 100%;
 
   @media (max-width: 768px) {
-    margin: 15px;
+    margin: 15px 15px 150px 15px;
   }
 
   @media (max-width: 480px) {
-    margin: 5px;
+    margin: 5px 5px 150px 5px;
   }
 }
 </style>
