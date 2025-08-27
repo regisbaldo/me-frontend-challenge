ee209f3ca89a49775f2ba3fe7c30ed9d58ce9f99
Reginaldo Simon Bonaldi <reginaldobonaldi22@gmail.com>
2025-08-26 23:16:10 -0300

    fix: fix app responsive padding and size

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,16 @@ import DetailContainer from './containers/DetailContainer.vue';
 
 <style lang="scss">
 .main-container {
-  margin: 25px 25px 150px 25px;
+  padding: 25px;
   background: $base;
-  height: 100%;
+  height: 100vh;
 
   @media (max-width: 768px) {
-    margin: 15px 15px 150px 15px;
+    padding: 15px;
   }
 
   @media (max-width: 480px) {
-    margin: 5px 5px 150px 5px;
+    padding: 5px;
   }
 }
 </style>
